11b121592ff21fa541e78f9aa8c7ee10c2992ddf
25r1116osawa-bit <25r1116.osawa@gmail.com>
2025-11-28 15:10:44 +0900

    更新

---
+++ 授業用/javascript/aaa/01_HellowWorld/javacsript問題集.md
@@ -0,0 +1,43 @@
+問題1 
+let a = 5;
+let b = 3;
+
+console.log(a + b);
+正解=8
+
+
+問題2 let name = "Taro";
+console.log("Hello, " + name + "!");
+正解=Hello, Taro!
+
+
+問題3（if文）
+let number = 10;
+
+if (number > 5) {
+  console.log("Big");
+} else {
+  console.log("Small");
+}
+正解＝Big
+
+
+問題4
+let fruits = ["apple", "banana", "orange"];
+
+console.log(fruits[1]);
+
+ヒント：配列は 0番目から 数えます。
+正解=banana
+
+
+問題5
+for (let i = 1; i <= 3; i++) {
+  console.log(i);
+}
+
+正解
+1
+2
+3
+
---
+++ 授業用/javascript/aaa/03_Function/main.js
@@ -0,0 +1,25 @@
+// 関数
+// プログラム(処理)の塊のことです。
+
+// click1という名前の関数を用意する
+function click1(){
+    // idがp1のtextContentに文字を代入
+    document.getElementById('p1').textContent = '実行ボタンが押されました。'
+}
+
+function click2(){
+    // idがp2のtextContentに文字を代入
+    document.getElementById('p2').textContent = '実行ボタンが押されました。'
+}
+
+function click3(){
+    // idがp1,p2のtextContentに文字を代入
+    document.getElementById('p1').textContent='1つ目の内容'
+    document.getElementById('p2').textContent='2つ目の内容'
+}
+
+
+//関数の書き方
+//function 関数名() {// // 処理}
+
+//
