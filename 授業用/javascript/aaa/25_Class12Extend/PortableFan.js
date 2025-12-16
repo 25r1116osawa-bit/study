bbb3cd77480891e61f911963358eb8f8bdda32a3
25r1116osawa-bit <25r1116.osawa@gmail.com>
2025-12-16 13:10:16 +0900

    更新

--- 授業用/javascript/aaa/25_Class12Extend/PortableFan.js
+++ 授業用/javascript/aaa/25_Class12Extend/PortableFan.js
@@ -1,17 +1,19 @@
-import Fan from "./Fan.js";
+import Fan from "./fan.js";
 
 // Fanを継承
-class PortableFan extends Fan{
+export default class PortableFan extends Fan{
 
-    // 新規プロパティ
-    #battery 
+    // 新規プロパティ(サブクラス専用プロパティ)
+    // バッテリー
+    #battery
 
     set battery(battery){
+        console.log('aaa'+battery)
+        console.log('bbb'+this.#battery)
         // Batteryバリデーション0 - 100
         if(0 <= battery && battery <= 100){
-        this.#battery = battery
+            this.#battery = battery
         }else{
-            // 赤文字ででるようになる。
             console.error('想定外の値が入力されました。')
         }
     }
@@ -20,9 +22,42 @@ class PortableFan extends Fan{
         return this.#battery
     }
 
-    // PortableFanのコンストラクタ
-    constructor(){
+    // PortableFanのコンストラクタ(Fanコンストラクタのオーバーライド)
+    constructor(output,blades=5){
+        // 親クラス(Fan)コンストラクタの呼び出し
+        super(output,blades)
+        
+        // 子クラス(PortableFan)の独自初期化処理
         this.#battery = 80
     }
 
+    // メソッドのオーバーライド(ポリモフィズム)
+    pressSwingButton(){
+        output.innerHTML += '首振り機能はありません' + '<br>'
+        // 親クラスのプライベートプロパティにはアクセスできない
+        // this.#swing = false
+        // アクセサーを利用する
+        this.swing = false
+    }
+
+    // 新規メソッド(サブクラス専用メソッド)
+    // 充電
+    charge(){
+        this.#battery = 100
+    }
+    
+    // メソッドのオーバーライド(ポリモフィズム)
+    blowing(){
+        console.log('ccc'+this.battery)
+        if(this.battery > 0){
+            if(super.blowing()){
+                this.battery = 0
+                output.innerHTML += `バッテリーの残量が0になりました。<br>`
+            }else{
+                output.innerHTML += `バッテリーの残量が減りませんでした。<br>`
+            }
+        }else{
+            output.innerHTML += `バッテリーの残量が有りません。<br>`
+        }
+    }
 }
--- 授業用/javascript/aaa/25_Class12Extend/main.js
+++ 授業用/javascript/aaa/25_Class12Extend/main.js
@@ -1,8 +1,9 @@
 // クラスについて
-// 継承編
+// ポリモフィズム編
 
 // モジュールのインポート(default)、標準モジュールの取り込み
-import Fan from "./Fan.js"
+import Fan from "./fan.js"
+import PortableFan from "./PortableFan.js"
 
 // モジュールのインポート、モジュール出力
 // import {Fan} from "./Fan.js"
@@ -14,19 +15,70 @@ let output = document.getElementById('output')
 // インスタンス化
 let fan01 = new Fan(output)
 let fan02 = new Fan(output,7)
+let fan03 = new PortableFan(output)
+let fan04 = new PortableFan(output,10)
 
 // fan01のメソッドの呼び出し
 output.innerHTML += fan01.swing + '<br>'
 fan01.pressSwingButton() + '<br>'
 output.innerHTML += fan01.swing + '<br>'
 output.innerHTML += fan01.blades + '<br>'
-
+fan01.blowing()
+fan01.pressPowerButton()
+fan01.changeWindPower()
+fan01.changeWindPower()
+fan01.blowing()
 
 // fan02のメソッドの呼び出し
 output.innerHTML += fan02.swing + '<br>'
 fan02.pressSwingButton() + '<br>'
 output.innerHTML += fan02.swing + '<br>'
-output.innerHTML += fan01.blades + '<br>'
+output.innerHTML += fan02.blades + '<br>'
+fan02.blowing()
+fan02.pressPowerButton()
+fan02.changeWindPower()
+fan02.changeWindPower()
+fan02.blowing()
+fan02.blowing()
+
+// fan03のメソッドの呼び出し
+output.innerHTML += fan03.swing + '<br>'
+fan03.pressSwingButton() + '<br>'
+output.innerHTML += fan03.swing + '<br>'
+output.innerHTML += fan03.blades + '<br>'
+fan03.blowing()
+fan03.pressPowerButton()
+fan03.changeWindPower()
+fan03.changeWindPower()
+fan03.blowing()
+fan03.blowing()
+
+// fan04のメソッドの呼び出し
+output.innerHTML += fan04.swing + '<br>'
+fan04.pressSwingButton() + '<br>'
+output.innerHTML += fan04.swing + '<br>'
+output.innerHTML += fan04.blades + '<br>'
+fan04.blowing()
+fan04.pressPowerButton()
+fan04.changeWindPower()
+fan04.changeWindPower()
+fan04.blowing()
+fan04.blowing()
+
+let fans = [new Fan(output),new Fan(output,3),new PortableFan(output),new PortableFan(output,12)]
+fans.forEach(fan => {
+    output.innerHTML += fan.swing + '<br>'
+    fan.pressSwingButton() + '<br>'
+    output.innerHTML += fan.swing + '<br>'
+    output.innerHTML += fan.blades + '<br>'
+    fan.blowing()
+    fan.pressPowerButton()
+    fan.changeWindPower()
+    fan.changeWindPower()
+    fan.blowing()
+    fan.blowing()
+});
 
 // クラスプロパティ(クラス変数)のアクセス
 Fan.infoFans()
+
