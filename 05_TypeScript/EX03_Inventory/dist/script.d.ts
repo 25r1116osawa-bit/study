b550e5efcdbba866c242a04e8e5688ae70c9e29b
25r1116osawa-bit <25r1116.osawa@gmail.com>
2026-01-06 11:37:35 +0900

    内容を更新

--- 05_TypeScript/EX03_Inventory/dist/script.d.ts
+++ 05_TypeScript/EX03_Inventory/dist/script.d.ts
@@ -1,9 +1,21 @@
+declare const productListData: {
+    id: string;
+    name: string;
+    price: number;
+    category: number;
+}[];
+declare const inventoryListData: {
+    id: string;
+    lotNumber: number;
+    storage: number;
+}[];
 declare const TAX = 0.1;
 declare enum Category {
     FOOD = "\u98DF\u54C1",
     DRINK = "\u98F2\u6599",
     DAILY = "\u65E5\u7528\u54C1"
 }
+declare const CURRENCY_UNIT = "\u5186";
 interface Product {
     id: string;
     name: string;
@@ -12,17 +24,34 @@ interface Product {
 }
 declare class Inventory implements Product {
     id: string;
-    productid: number;
+    lotNumber: number;
     name: string;
     storage: number;
     price: number;
     category: Category;
     static number: number;
-    constructor(id: string, productid: number, name: string, storage: number, price: number, category: Category);
+    constructor(id: string, name: string, storage: number, price: number, category: Category);
     sales(): {
         excludingTax: number;
         includingTax: number;
         taxAmount: number;
     };
+    dispInfo(): void;
 }
+declare class InventoryList {
+    inventoryList: Inventory[];
+    constructor(pList: {
+        id: string;
+        name: string;
+        price: number;
+        category: Category | number;
+    }[], iList: {
+        id: string;
+        lotNumber: number;
+        storage: number;
+    }[]);
+    dispList(): void;
+}
+declare const inventoryList: InventoryList;
+declare const categoryElement: HTMLSelectElement;
 //# sourceMappingURL=script.d.ts.map
