78f4b85d90b1243767a81b827bc67884390982de
25r1116osawa-bit <25r1116.osawa@gmail.com>
2026-01-23 16:02:30 +0900

    ファイルを更新

--- 07_Next.js/next-sample/__Tests__/Text.test.tsx
+++ 07_Next.js/next-sample/__Tests__/Text.test.tsx
@@ -1,10 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import Text from '@/app/components/Text' // default export に合わせる
 
-describe('Text component', () => {
-  it('正しいメッセージが表示されること', () => {
-    render(<Text text="あいうえお" />)
-    const heading = screen.getByText('あいうえお')
-    expect(heading).toBeInTheDocument() // jest-dom の拡張
-  })
+describe('Text Component', () => {
+    // 今からどのようなテストを行うのか？
+    it('正しいメッセージが表示されること',()=>{
+        // レンダリング実行
+        render(<Text text="あいうえお" />)
+        // 出力パターン生成　今回は完全一致
+        const heading = screen.getByText(/あいうえお/)
+        // 結果の確認
+        expect(heading).toBeInTheDocument()
+    })
 })
